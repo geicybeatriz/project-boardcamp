78a77c6476a44f0a66b6e46e123b0e0d88e90471
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-05-29 04:15:41 -0300

    feat: add register new game endpoint

--- Controllers/gamesController.js
+++ Controllers/gamesController.js
@@ -1,11 +1,25 @@
 import connection from "./../db.js";
 
 export async function getGames(req, res){
+    
 
 
 }
 
 export async function addGame(req, res){
+    console.log(req.body);
+    const {name, image, stockTotal, categoryId, pricePerDay} = req.body;
     
+    try {
+        const registerGame = await connection.query(`
+        INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay")
+        VALUES ($1, $2, $3, $4, $5);`, 
+        [name, image, stockTotal, categoryId, pricePerDay]);
+
+        res.status(201);
+    } catch (error) {
+        console.log("erro", error);
+        res.status(500).send("erro ao cadastrar jogo");
+    }
 
 }
--- Routes/gamesRoute.js
+++ Routes/gamesRoute.js
@@ -1,10 +1,10 @@
-import { getGames } from "../Controllers/gamesController.js";
+import { addGame, getGames } from "../Controllers/gamesController.js";
 import { gameValidation } from "../Middlewares/gamesValidation.js";
 import { checkGameExistence } from "../Middlewares/checkGame.js";
 import { Router } from "express";
 
 const gameController = Router();
 gameController.get("/games", getGames);
-gameController.post("/games", gameValidation, checkGameExistence);
+gameController.post("/games", gameValidation, checkGameExistence, addGame);
 
 export default gameController;
