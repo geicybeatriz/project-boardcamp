70fd0ad03ca2587cd39a363d6625e50cb540d8b9
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-05-29 18:38:00 -0300

    feat: add getting games by name at endpoint

---
+++ Controllers/customersController.js
@@ -0,0 +1,30 @@
+import connection from "./../db.js";
+
+//TODO: criar função que valida os dados do cliente, 
+// pesquisar por regex que possam ajudar no cpf, telefone
+//estrutura do cliente
+// procurar por query strings, pq eu esqueci kkkkkkkk
+const customer = {
+    id: 1,
+    name: 'João Alfredo',
+    phone: '21998899222',
+    cpf: '01234567890',
+    birthday: '1992-10-05'
+}
+
+// criar funções para o endpoint
+export async function getAllCustomers(req, res){
+
+}
+
+export async function getCustomerById(req, res){
+    
+}
+
+export async function addCustomer(req, res){
+
+}
+
+export async function updateCustomer(req, res){
+
+}
--- Controllers/gamesController.js
+++ Controllers/gamesController.js
@@ -1,13 +1,31 @@
 import connection from "./../db.js";
 
 export async function getGames(req, res){
+    const nameGame = req.query.name;
+    console.log("recebo", nameGame);
+
+    const filteredGame = [];
+
     try {
         const games = await connection.query(`
             SELECT games.*, categories.name as "categoryName" 
             FROM games 
             JOIN categories 
             ON games."categoryId" = categories.id;`);
-        res.status(200).send(games.rows);
+
+        if (nameGame){
+            let game = new RegExp(`^${nameGame.toLowerCase()}`);
+            for (let i = 0; i < games.rows.length; i++){
+                if(game.test(games.rows[i].name.toLowerCase())){
+                    filteredGame.push(games.rows[i]);
+                }
+            }
+            res.status(200).send(filteredGame);
+        }
+        else{
+            res.status(200).send(games.rows);
+        }
+
     } catch (error) {
         console.log("erro", error);
         res.status(500).send("erro ao buscar jogos");
